42e467d8c75c5ed1b09707a23bbf4bb12cc98847
Alex_meneses <a.menesess@uniandes.edu.co>
2023-11-05 00:55:15 -0500

    Se crea clase cafe con atributos

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cafe } from './cafe';
 
 @Component({
   selector: 'app-cafe',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CafeComponent implements OnInit {
 
+  private cafes: Array<Cafe> = [];
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/cafe/cafe.ts
@@ -0,0 +1,11 @@
+export class Cafe {
+    nombre: string;
+    tipo: string;
+    region: string;
+
+    public constructor(nombre: string, tipo: string, region: string) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.region = region;
+    }
+}
